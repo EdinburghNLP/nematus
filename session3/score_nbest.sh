405e681b7bb193b35408c18dd1b2971f444695db
orhanf <orhan.firat@ceng.metu.edu.tr>
2015-10-20 04:58:07 -0400

    change scoring script filenames

--- session3/score_nbest.sh
+++ session3/score_nbest.sh
@@ -13,6 +13,6 @@ python ./rescore_with_lm.py -n -b 0.5 \
 	${HOME}/models/model_session0.npz.pkl \
 	${HOME}/data/wiki.tok.txt.gz.pkl \
 	${HOME}/data/europarl-v7.fr-en.en.tok.pkl \
-	./newstest2011.trans.en.tok.SMALL \
-    ./newstest2011.trans.en.tok.SMALL.rescored
+	./newstest2011.trans.en.tok \
+    ./newstest2011.trans.en.tok.rescored
 
