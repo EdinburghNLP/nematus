9063de7f7998c556b96b96e1f9d0318da1102f4e
Joost Bastings <joost.bastings@gmail.com>
2015-10-24 08:55:58 +0100

    setup script now executes from home directory and installs Theano

--- data/setup_cluster_env.sh
+++ data/setup_cluster_env.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 # This script sets up development and data environments for 
 # fionn cluster, copy under your home directory and run.
 
@@ -20,18 +20,29 @@ DATA_DIR=${HOME}/data
 # our trained models will be saved here
 MODELS_DIR=${HOME}/models
 
+# theano repository
+THEANO_GIT=https://github.com/Theano/Theano.git
+
+# theano install dir
+THEANO_DIR=${HOME}/repo/Theano
+
+# move to home directory
+cd
 
 # copy dependency file to your local and extract 
+echo "Copying and extracting dependency file"
 rsync --bwlimit=20000 -Pavz ${LOCAL_INSTALL_FILE} ${HOME}
 tar zxvf ${HOME}/local_install.tgz
 
 # clone the repository from github into code directory
+echo "Cloning lab repository"
 if [ ! -d "${CODE_DIR}" ]; then
     mkdir -p ${CODE_DIR}
 fi
 git clone ${CODE_CENTRAL} ${CODE_DIR}
 
 # copy corpora, dictionaries etc for training and dev
+echo "Copying data"
 if [ ! -d "${DATA_DIR}" ]; then
     mkdir -p ${DATA_DIR}
 fi
@@ -42,6 +53,13 @@ rsync --bwlimit=20000 -Pavz ${REF_DATA_DIR}/news* ${DATA_DIR}
 if [ ! -d "${MODELS_DIR}" ]; then
     mkdir -p ${MODELS_DIR}
 fi
+
+# clone and install Theano
+echo "Cloning/installing Theano"
+mkdir -p ${THEANO_DIR}
+git clone ${THEANO_GIT} ${THEANO_DIR}
+cd ${THEANO_DIR}
+python setup.py install --user
 
 # check if theano is working
 python -c "import theano;print 'theano available!'"
