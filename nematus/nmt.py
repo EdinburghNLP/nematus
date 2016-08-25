c71f113d6987017416b227959333d402739f753e
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2016-08-25 19:08:55 +0100

    Numerically more stable softmax for attention. Thanks Joost!
    Seems to fix NaN issues we encountered when models converge, especially with Adam.

--- nematus/nmt.py
+++ nematus/nmt.py
@@ -484,7 +484,7 @@ def gru_cond_layer(tparams, state_below, options, prefix='gru',
         pctx__ = tensor.tanh(pctx__)
         alpha = tensor.dot(pctx__*ctx_dropout[1], U_att)+c_tt
         alpha = alpha.reshape([alpha.shape[0], alpha.shape[1]])
-        alpha = tensor.exp(alpha)
+        alpha = tensor.exp(alpha - alpha.max(0, keepdims=True))
         if context_mask:
             alpha = alpha * context_mask
         alpha = alpha / alpha.sum(0, keepdims=True)
