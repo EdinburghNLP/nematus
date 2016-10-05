03856cee96b2e942a051819ff453fb22eea6aa5d
Julian Hitschler <hitschler@cl.uni-heidelberg.de>
2016-10-05 13:45:14 +0200

    ChrF consistent with reference implementation.

--- nematus/metrics/test_chrf.py
+++ nematus/metrics/test_chrf.py
@@ -16,42 +16,49 @@ class TestCharacterFScoreReference(unittest.TestCase):
         segment = self.tokenize("Consistency is the last refuge of the unimaginative")
         scorer = CharacterFScorer('n=6,beta=3')
         scorer.set_reference(segment)
-        self.assertEqual(scorer.score(segment), 1.0)
+        self.assertEqual(scorer.score(segment), 1.0)   
     def test_completely_different_segments(self):
-        segment_a = self.tokenize("A A A")
-        segment_b = self.tokenize("B B B")
+        segment_a = self.tokenize("AAAAAA")
+        segment_b = self.tokenize("BBBB")
         scorer = CharacterFScorer('n=3,beta=3')
         scorer.set_reference(segment_a)
         self.assertEqual(scorer.score(segment_b), 0.0)
-    def test_almost_correct(self):
-        segment_a = self.tokenize("foo bar")
-        segment_b = self.tokenize("foo bat")
-        scorer = CharacterFScorer('n=6,beta=1')
-        scorer.set_reference(segment_a)
-        self.assertEqual(scorer.score(segment_b), 0.5)
-    def test_almost_correct_asym(self):
-        segment_a = self.tokenize("foofoofoot")
-        segment_b = self.tokenize("foofoofoo")     
-        scorer = CharacterFScorer('n=6,beta=3')
-        scorer.set_reference(segment_a)
-        self.assertEqual(scorer.score(segment_b), 10*((1*0.8)/((9*1)+0.8)))
-    def test_too_short_correct(self):
-        segment_a = self.tokenize("foofo")
-        segment_b = self.tokenize("foofo")     
+    def test_empty_string(self):
+        segment_a = self.tokenize("")
+        segment_b = self.tokenize("")
         scorer = CharacterFScorer('n=6,beta=3')
         scorer.set_reference(segment_a)
         self.assertEqual(scorer.score(segment_b), 1.0)
-    def test_too_short_incorrect_0(self):
-        segment_a = self.tokenize("foofo")
-        segment_b = self.tokenize("foobar")     
+    def test_one_character_empty_string(self):
+        segment_a = self.tokenize("A")
+        segment_b = self.tokenize("")
         scorer = CharacterFScorer('n=6,beta=3')
         scorer.set_reference(segment_a)
-    def test_too_short_incorrect_1(self):
-        segment_a = self.tokenize("foobar")
-        segment_b = self.tokenize("foofo")     
+        self.assertEqual(scorer.score(segment_b), 0.0)
+    def test_empty_string_one_character(self):
+        segment_a = self.tokenize("")
+        segment_b = self.tokenize("A")
         scorer = CharacterFScorer('n=6,beta=3')
         scorer.set_reference(segment_a)
-        self.assertEqual(scorer.score(segment_b), 0.0)    
-
+        self.assertEqual(scorer.score(segment_b), 0.0)
+    def test_half_right(self):
+        segment_a = self.tokenize("AB")
+        segment_b = self.tokenize("AA")
+        scorer = CharacterFScorer('n=6,beta=3')
+        scorer.set_reference(segment_a)
+        self.assertEqual(scorer.score(segment_b), 0.25)                     
+    def test_one_character(self):
+        segment_a = self.tokenize("A")
+        segment_b = self.tokenize("A")
+        scorer = CharacterFScorer('n=6,beta=3')
+        scorer.set_reference(segment_a)
+        self.assertEqual(scorer.score(segment_b), 1.0)
+    def test_almost_correct(self):
+        segment_a = self.tokenize("risk assessment has to be undertaken by those who are qualified and expert in that area - that is the scientists .")
+        segment_b = self.tokenize(" risk assessment must be made of those who are qualified and expertise in the sector - these are the scientists .")
+        scorer = CharacterFScorer('n=6,beta=3')
+        scorer.set_reference(segment_a)
+        self.assertEqual('{0:.12f}'.format(scorer.score(segment_b)), "0.652414427449")
+    
 if __name__ == '__main__':
     unittest.main()
