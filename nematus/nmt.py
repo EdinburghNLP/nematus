019aff4402b901203f2c45a41942c4a26eca0864
Antonio Valerio Miceli Barone <amiceli@hodor.inf.ed.ac.uk>
2017-02-14 16:22:29 +0000

    gradient truncation in BPTT

--- nematus/nmt.py
+++ nematus/nmt.py
@@ -226,6 +226,7 @@ def build_encoder(tparams, options, trng, use_noise, x_mask=None, sampling=False
                                             mask=x_mask,
                                             emb_dropout=emb_dropout,
                                             rec_dropout=rec_dropout,
+                                            truncate_gradient=options['encoder_truncate_gradient'],
                                             profile=profile)
 
 
@@ -243,6 +244,7 @@ def build_encoder(tparams, options, trng, use_noise, x_mask=None, sampling=False
                                              mask=xr_mask,
                                              emb_dropout=emb_dropout_r,
                                              rec_dropout=rec_dropout_r,
+                                             truncate_gradient=options['encoder_truncate_gradient'],
                                              profile=profile)
 
     # context will be the concatenation of forward and backward rnns
@@ -323,6 +325,7 @@ def build_model(tparams, options):
                                             emb_dropout=emb_dropout_d,
                                             ctx_dropout=ctx_dropout_d,
                                             rec_dropout=rec_dropout_d,
+                                            truncate_gradient=options['decoder_truncate_gradient'],
                                             profile=profile)
     # hidden states of the decoder gru
     proj_h = proj[0]
@@ -428,6 +431,7 @@ def build_sampler(tparams, options, use_noise, trng, return_alignment=False):
                                             emb_dropout=emb_dropout_d,
                                             ctx_dropout=ctx_dropout_d,
                                             rec_dropout=rec_dropout_d,
+                                            truncate_gradient=options['decoder_truncate_gradient'],
                                             profile=profile)
     # get the next hidden state
     next_state = proj[0]
@@ -664,7 +668,7 @@ def build_full_sampler(tparams, options, use_noise, trng, greedy=False):
     (sample, state, probs), updates = theano.scan(decoder_step,
                         outputs_info=[init_w, init_state, None],
                         non_sequences=[ctx, pctx_, target_dropout, emb_dropout_d, rec_dropout_d, ctx_dropout_d]+shared_vars,
-                        n_steps=n_steps)
+                        n_steps=n_steps, truncate_gradient=options['decoder_truncate_gradient'],)
 
     print >>sys.stderr, 'Building f_sample...',
     if greedy:
@@ -992,6 +996,8 @@ def train(dim_word=512,  # word vector dimensionality
           prior_model=None, # Prior model file, used for MAP
           tie_encoder_decoder_embeddings=False, # Tie the input embeddings of the encoder and the decoder (first factor only)
           tie_decoder_embeddings=False, # Tie the input embeddings of the decoder with the softmax output embeddings
+          encoder_truncate_gradient=-1, # Truncate BPTT gradients in the encoder to this value. Use -1 for no truncation
+          decoder_truncate_gradient=-1, # Truncate BPTT gradients in the decoder to this value. Use -1 for no truncation
     ):
 
     # Model options
@@ -1593,6 +1599,10 @@ if __name__ == '__main__':
                          help='size of maxibatch (number of minibatches that are sorted by length) (default: %(default)s)')
     training.add_argument('--objective', choices=['CE', 'MRT'], default='CE',
                          help='training objective. CE: cross-entropy minimization (default); MRT: Minimum Risk Training (https://www.aclweb.org/anthology/P/P16/P16-1159.pdf)')
+    training.add_argument('--encoder_truncate_gradient', type=int, default=-1, metavar='INT',
+                         help="truncate BPTT gradients in the encoder to this value. Use -1 for no truncation (default: %(default)s)")
+    training.add_argument('--decoder_truncate_gradient', type=int, default=-1, metavar='INT',
+                         help="truncate BPTT gradients in the encoder to this value. Use -1 for no truncation (default: %(default)s)")
 
     finetune = training.add_mutually_exclusive_group()
     finetune.add_argument('--finetune', action="store_true",
