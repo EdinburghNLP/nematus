79bdefec7e8c5726d6afde79b32806d40e2935d4
Jozef Mokry <jozef.mokry@gmail.com>
2017-02-19 21:18:41 +0000

    build_model is now free of interpreter errors

---
+++ nematus/tf_initializers.py
@@ -0,0 +1,20 @@
+'''
+Parameter initializers
+'''
+
+import numpy
+
+def ortho_weight(ndim):
+    W = numpy.random.randn(ndim, ndim)
+    u, s, v = numpy.linalg.svd(W)
+    return u.astype('float32')
+
+def norm_weight(nin, nout=None, scale=0.01, ortho=True):
+    if nout is None:
+        nout = nin
+    if nout == nin and ortho:
+        W = ortho_weight(nin)
+    else:
+        W = scale * numpy.random.randn(nin, nout)
+    return W.astype('float32')
+
---
+++ nematus/tf_layers.py
@@ -0,0 +1,177 @@
+"""
+Layer definitions
+"""
+
+from tf_initializers import ortho_weight, norm_weight
+import tensorflow as tf
+import numpy 
+
+def matmul3d(x3d, matrix):
+    shape = tf.shape(x3d)
+    mat_shape = tf.shape(matrix)
+    x2d = tf.reshape(x3d, [shape[0]*shape[1], shape[2]])
+    result2d = tf.matmul(x2d, matrix)
+    result3d = tf.reshape(x2d, [shape[0], shape[1], mat_shape[1]])
+    return result3d
+
+class FeedForwardLayer(object):
+    def __init__(self,
+                 in_size,
+                 out_size,
+                 non_linearity=tf.nn.tanh,
+                 input_is_3d=False):
+        self.W = tf.Variable(norm_weight(in_size, out_size), name='W')
+        self.b = tf.Variable(numpy.zeros((out_size,)).astype('float32'), name='b')
+        self.non_linearity = non_linearity
+        self.input_is_3d = input_is_3d
+
+    def forward(self, x):
+        if self.input_is_3d:
+            y = matmul3d(x, self.W) + self.b
+        else:
+            y = tf.matmul(x, self.W) + self.b
+        y = self.non_linearity(y)
+        return y
+
+class EmbeddingLayer(object):
+    def __init__(self,
+                 vocabulary_size,
+                 embedding_size):
+        self.embeddings = tf.Variable(norm_weight(vocabulary_size, embedding_size),
+                                      name='embeddings')
+    
+    def forward(self, x):
+        embs = tf.nn.embedding_lookup(self.embeddings, x)
+        return embs
+
+class RecurrentLayer(object):
+    def __init__(self,
+                 initial_state,
+                 step_fn):
+        self.initial_state = initial_state
+        self.step_fn = step_fn
+
+    def forward(self, x):
+        # Assumes that x has shape: time, batch, ...
+        states = tf.scan(fn=self.step_fn,
+                         elems=x,
+                         initializer=self.initial_state)
+        return states
+    
+class GRUStep(object):
+    def __init__(self, 
+                 input_size, 
+                 state_size):
+        self.state_to_gates = tf.Variable(
+                                numpy.concatenate(
+                                    [ortho_weight(state_size),
+                                     ortho_weight(state_size)],
+                                    axis=1), 
+                                name='state_to_gates')
+        self.input_to_gates = tf.Variable(
+                                numpy.concatenate(
+                                    [norm_weight(input_size, state_size),
+                                     norm_weight(input_size, state_size)],
+                                    axis=1), 
+                                name='input_to_gates')
+        self.gates_bias = tf.Variable(
+                            numpy.zeros((2*state_size,)).astype('float32'),
+                            name='gates_bias')
+
+        self.state_to_proposal = tf.Variable(
+                                    ortho_weight(state_size),
+                                    name = 'state_to_proposal')
+        self.input_to_proposal = tf.Variable(
+                                    norm_weight(input_size, state_size),
+                                    name = 'input_to_proposal')
+        self.proposal_bias = tf.Variable(
+                                    numpy.zeros((state_size,)).astype('float32'),
+                                    name='proposal_bias')
+
+    def forward(self, prev_state, x):
+        gates = tf.matmul(prev_state, self.state_to_gates)
+        #TODO: x*self.input_to_gates can be done in one matmul
+        # for all x; same for x*self.input_to_proposal
+        gates += tf.matmul(x, self.input_to_gates)
+        gates += self.gates_bias
+        gates = tf.nn.sigmoid(gates)
+        read_gate, update_gate = tf.split(gates,
+                                          num_or_size_splits=2,
+                                          axis=1)
+
+        proposal = tf.matmul(prev_state, self.state_to_proposal)
+        proposal *= read_gate
+        proposal += tf.matmul(x, self.input_to_proposal)
+        proposal += self.proposal_bias
+        proposal  = tf.tanh(proposal)
+
+        new_state = update_gate*prev_state + (1-update_gate)*proposal
+
+        return new_state
+
+class AttentionStep(object):
+    def __init__(self,
+                 context,
+                 context_state_size,
+                 context_mask,
+                 state_size,
+                 hidden_size):
+        self.state_to_hidden = tf.Variable(
+                                norm_weight(state_size, hidden_size),
+                                name='state_to_hidden')
+        self.context_to_hidden = tf.Variable(
+                                norm_weight(context_state_size, hidden_size),
+                                name='context_to_hidden')
+        self.hidden_bias = tf.Variable(
+                            numpy.zeros((hidden_size,)).astype('float32'),
+                            name='hidden_bias')
+        self.hidden_to_score = tf.Variable(
+                                norm_weight(hidden_size, 1),
+                                name='hidden_to_score')
+        
+        self.context = context
+        self.context_mask = context_mask
+
+        # precompute these activations, they are the same at each step
+        # Ideally the compiler would have figured out that too
+        self.hidden_from_context = matmul3d(context, self.context_to_hidden)
+        self.hidden_from_context += self.hidden_bias
+
+    def forward(self, prev_state):
+        hidden = self.hidden_from_context
+        hidden += tf.matmul(prev_state, self.state_to_hidden)
+        hidden = tf.nn.tanh(hidden)
+        # context has shape seqLen x batch x context_state_size
+        # mask has shape seqLen x batch
+
+        scores = matmul3d(hidden, self.hidden_to_score) # seqLen x batch x 1
+        scores = tf.squeeze(scores, axis=2)
+        scores = scores - tf.reduce_max(scores, axis=0, keep_dims=True)
+        scores = tf.exp(scores)
+        scores *= self.context_mask
+        scores = scores / tf.reduce_sum(scores, axis=0, keep_dims=True)
+
+        attention_context = self.context * tf.expand_dims(scores, axis=2)
+        attention_context = tf.reduce_sum(attention_context, axis=0, keep_dims=False)
+
+        return attention_context
+        
+
+        
+
+
+        
+
+                                    
+
+        
+
+                                
+
+
+
+
+
+
+    
+
