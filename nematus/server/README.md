1209042382c6a4c7e44dbc7fcabdd45ea5616b45
Samuel Läubli <laeubli@ifi.uzh.ch>
2017-05-10 16:49:00 +0200

    define basic Nematus API style

---
+++ nematus/server/README.md
@@ -0,0 +1,45 @@
+# Nematus Server
+Runs Nematus as a Web Server. Further details tba.
+
+# API
+Nematus Server supports several API styles.
+
+## Nematus Translation API
+
+### HTTP Request
+
+`POST http://host:port/translate`
+
+Content-Type: application/json
+
+### Query Parameters
+
+| Parameter           | Type                  | Description  |
+| --------------------|-----------------------| -------------|
+| ``segments``        | ``array(str)``        | The sentences to be translated (source language). |
+| ``normalize``       | ``boolean``           | Normalise scores by sentence length. Default: ``true``. |
+| ``beam_width``      | ``int``               | The beam width to be used for decoding. Default: ``5``. |
+| ``character_level`` | ``boolean``           | Enables character- rather than subword-level translation. Default: ``false``. |
+| ``n_best``          | ``int``               | Return n best translations per segment. Default: ``1``. |
+| ``return_word_alignment`` | ``boolean``     | Return word alignment (source to target language) for each segment. Default: ``false``. |
+| ``return_word_probabilities`` | ``boolean`` | Return the probability of each word (target language) for each segment. Default: ``false``. |
+
+### Response Body
+
+If successful, the response body contains a JSON object with the following structure:
+
+```json
+{
+  "status": "ok",
+  "data": [
+    {
+      "translation": "Translation of first segment."
+    },
+    {
+      "translation": "Translation of second segment."
+    }
+  ]
+}
+```
+
+Additional fields for each segment in ``data`` are ``word_alignment`` and ``word_probabilities`` (details tba).
---
+++ nematus/server/api/nematus_style.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Defines the Nematus API for translation requests and responses.
+"""
+
+import json
+from ..request import TranslationRequest
+from ..response import TranslationResponse
+
+class TranslationRequestNematus(TranslationRequest):
+    def _parse(self):
+        request = self._request.json
+        if 'segments' in request:
+            self.segments = request['segments']
+        if 'beam_width' in request:
+            self.beam_width = request['beam_width']
+        if 'normalize' in request:
+            self.normalize = request['normalize']
+        if 'character_level' in request:
+            self.character_level = request['character_level']
+        if 'return_word_alignment' in request:
+            self.return_word_alignment = request['return_word_alignment']
+        if 'return_word_probabilities' in request:
+            self.return_word_alignment = request['return_word_probabilities']
+
+class TranslationResponseNematus(TranslationResponse):
+    def _format(self):
+        response = {
+            'status': '',
+            'data': [],
+        }
+        if self._status == self.STATUS_OK:
+            response['status'] = 'ok'
+            for i, translation in enumerate(self._segments):
+                segment = {'translation': translation}
+                if self._word_alignments:
+                    segment['word_alignment'] = self._word_alignments[i]
+                if self._word_probabilities:
+                    segment['word_probabilities'] = self._word_probabilities[i]
+                response['data'].append(segment)
+        else:
+            response['status'] = 'error'
+        return json.dumps(response)
