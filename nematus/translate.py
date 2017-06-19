9ca58f48f2c5d2c52a4e5a60a44ff697b8c532b2
Mathias Mueller <mathias.mueller@uzh.ch>
2017-06-19 11:03:03 +0200

    request ID for queue items, collect output in a flexible way

--- nematus/translate.py
+++ nematus/translate.py
@@ -3,19 +3,18 @@
 Translates a source file using a translation model.
 '''
 import sys
-import argparse
 import numpy
 import json
+import os
+
+from multiprocessing import Process, Queue
+from collections import defaultdict
+from Queue import Empty
 
 from util import load_dict, load_config
 from compat import fill_options
 from hypgraph import HypGraphRenderer
 from console import ConsoleInterfaceDefault
-
-from multiprocessing import Process, Queue, Value
-from Queue import Empty
-from ctypes import c_bool
-from cStringIO import StringIO
 
 class Translation(object):
     #TODO move to separate file?
@@ -64,9 +63,6 @@ class QueueItem(object):
         self.__dict__.update(kwargs)
 
 class Translator(object):
-    """
-    Loads trained models and translates.
-    """
 
     def __init__(self, decoder_settings):
         """
@@ -76,17 +72,16 @@ class Translator(object):
         self._num_processes = decoder_settings.num_processes
         self._device_list = decoder_settings.device_list
         self._verbose = decoder_settings.verbose
-        self._shutdown = Value(c_bool, False)
+        self._retrieved_translations = defaultdict(dict)
 
         # load model options
         self._load_model_options()
         # load and invert dictionaries
         self._build_dictionaries()
-        # load translation model
-        # work around early binding to GPU device, commented out for now
-        # self._load_models()
-        # set up FIFO queues and processes
-        self._start_queues_processes()
+        # set up queues
+        self._init_queues()
+        # init worker processes
+        self._init_processes()
 
     def _load_model_options(self):
         """
@@ -140,15 +135,51 @@ class Translator(object):
 
         self._word_idict_trg = word_idict_trg
 
-    def _load_models(self):
+    def _init_queues(self):
+        """
+        Sets up shared queues for inter-process communication.
+        """
+        self._input_queue = Queue()
+        self._output_queue = Queue()
+
+    def shutdown(self):
+        """
+        Executed from parent process to terminate workers,
+        method: "poison pill".
+        """
+        for process in self._processes:
+            self._input_queue.put(None)
+
+    def _init_processes(self):
+        """
+        Starts child (worker) processes.
+        """
+        processes = [None] * self._num_processes
+        for process_id in xrange(self._num_processes):
+            deviceid = ''
+            if self._device_list is not None and len(self._device_list) != 0:
+                deviceid = self._device_list[process_id % len(self._device_list)].strip()
+            processes[process_id] = Process(
+                target=self._start_worker,
+                args=(process_id, deviceid)
+                )
+            processes[process_id].start()
+
+        self._processes = processes
+
+
+    ### MODEL LOADING AND TRANSLATION IN CHILD PROCESS ###
+
+    def _load_theano(self):
         """
         Loads models, sets theano shared variables and builds samplers.
+        This entails irrevocable binding to a specific GPU!
         """
 
         from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
         from theano import shared
 
-        from nmt import build_sampler
+        from nmt import (build_sampler, gen_sample)
         from theano_util import (load_params, init_theano_params)
 
         trng = RandomStreams(1234)
@@ -171,39 +202,13 @@ class Translator(object):
             fs_init.append(f_init)
             fs_next.append(f_next)
 
-        self._trng = trng
-        self._fs_init = fs_init
-        self._fs_next = fs_next
+        return trng, fs_init, fs_next, gen_sample
 
-
-    def _start_queues_processes(self):
+    def _set_device(self, device_id):
         """
-        Sets up input and output queues, starts workers.
+        Modifies environment variable to change the THEANO device.
         """
-        self._input_queue = Queue()
-        self._output_queue = Queue()
-
-        self._processes = [None] * self._num_processes
-
-        for process_id in xrange(self._num_processes):
-            device_id = ''
-            if self._device_list is not None and len(self._device_list) != 0:
-                device_id = self._device_list[process_id % len(self._device_list)].strip()
-
-
-            self._processes[process_id] = Process(target=self._translate,
-                                                  args=(process_id, device_id))  # todo: timeout?
-            self._processes[process_id].start()
-
-
-    def _translate(self, process_id, device_id, timeout=1):
-        """
-        Actual translation (model sampling).
-        """
-
-        # if the --device-list argument is set
         if device_id != '':
-            import os
             theano_flags = os.environ['THEANO_FLAGS'].split(',')
             exist = False
             for i in xrange(len(theano_flags)):
@@ -211,136 +216,101 @@ class Translator(object):
                     exist = True
                     theano_flags[i] = '%s=%s' % ('device', device_id)
                     break
-            if exist == False:
+            if exist is False:
                 theano_flags.append('%s=%s' % ('device', device_id))
             os.environ['THEANO_FLAGS'] = ','.join(theano_flags)
 
-        self._load_models()
+    def _load_models(self, device_id):
+        """
+        Modifies environment variable to change the THEANO device, then loads
+        models and returns them.
+        """
+        if self._verbose:
+            sys.stderr.write("Process '%s' - Loading models on GPU %s\n" % (os.getpid(), device_id))
 
+        # modify environment flag 'device'
+        self._set_device(device_id)
+
+        # build and return models
+        return self._load_theano()
+
+    def _start_worker(self, process_id, device_id):
+        """
+        Function executed by each worker once started. Do not execute in
+        the parent process.
+        """
+        # load theano functionality
+        trng, fs_init, fs_next, gen_sample = self._load_models(device_id)
+
+        # listen to queue in while loop, translate items
         while True:
-            queue_item = None
+            input_item = self._input_queue.get()
 
-            try:
-                queue_item = self._input_queue.get(True, timeout=timeout)
-            except Empty:
-                if self._shutdown.value == True: break
+            if input_item is None:
+                break
+            idx = input_item.idx
+            request_id = input_item.request_id
 
-            if queue_item:
-                # unpack queue item attributes
-                verbose = queue_item.verbose
-                normalize = queue_item.normalize
-                nbest = queue_item.nbest
-                idx = queue_item.idx
+            output_item = self._translate(process_id, input_item, trng, fs_init, fs_next, gen_sample)
+            self._output_queue.put((request_id, idx, output_item))
 
-                # logging
-                if verbose:
-                    sys.stderr.write('{0} - {1}\n'.format(process_id, idx))
-
-                # sample given an input sequence and obtain scores
-                sample, score, word_probs, alignment, hyp_graph = self._sample(queue_item)
-
-                # normalize scores according to sequence lengths
-                if normalize:
-                    lengths = numpy.array([len(s) for s in sample])
-                    score = score / lengths
-                if nbest:
-                    item = sample, score, word_probs, alignment, hyp_graph
-                else:
-                    # return translation with lowest score only
-                    sidx = numpy.argmin(score)
-                    item = sample[sidx], score[sidx], word_probs[sidx], alignment[sidx], hyp_graph
-
-                # put output item into queue
-                self._output_queue.put((idx,item))
         return
 
-    def _sample(self, queue_item):
+    def _translate(self, process_id, input_item, trng, fs_init, fs_next, gen_sample):
+        """
+        Actual translation (model sampling).
+        """
+
+        # unpack input item attributes
+        verbose = input_item.verbose
+        normalize = input_item.normalize
+        nbest = input_item.nbest
+        idx = input_item.idx
+
+        # logging
+        if verbose:
+            sys.stderr.write('{0} - {1}\n'.format(process_id, idx))
+
+        # sample given an input sequence and obtain scores
+        sample, score, word_probs, alignment, hyp_graph = self._sample(input_item, trng, fs_init, fs_next, gen_sample)
+
+        # normalize scores according to sequence lengths
+        if normalize:
+            lengths = numpy.array([len(s) for s in sample])
+            score = score / lengths
+        if nbest > 1:
+            output_item = sample, score, word_probs, alignment, hyp_graph
+        else:
+            # return translation with lowest score only
+            sidx = numpy.argmin(score)
+            output_item = sample[sidx], score[sidx], word_probs[
+                sidx], alignment[sidx], hyp_graph
+
+        return output_item
+
+    def _sample(self, input_item, trng, fs_init, fs_next, gen_sample):
         """
         Sample from model.
         """
-        from nmt import gen_sample
 
-        # unpack queue item attributes
-        return_hyp_graph = queue_item.return_hyp_graph
-        return_alignment = queue_item.return_alignment
-        suppress_unk = queue_item.suppress_unk
-        k = queue_item.k
-        seq = queue_item.seq
+        # unpack input item attributes
+        return_hyp_graph = input_item.return_hyp_graph
+        return_alignment = input_item.return_alignment
+        suppress_unk = input_item.suppress_unk
+        k = input_item.k
+        seq = input_item.seq
 
-        return gen_sample(self._fs_init, self._fs_next,
+        return gen_sample(fs_init, fs_next,
                           numpy.array(seq).T.reshape(
                               [len(seq[0]), len(seq), 1]),
-                          trng=self._trng, k=k, maxlen=200,
+                          trng=trng, k=k, maxlen=200,
                           stochastic=False, argmax=False,
                           return_alignment=return_alignment,
                           suppress_unk=suppress_unk,
                           return_hyp_graph=return_hyp_graph)
 
-    def _send_jobs(self, input_, translation_settings):
-        """
-        """
-        source_sentences = []
-        for idx, line in enumerate(input_):
-            if translation_settings.char_level:
-                words = list(line.decode('utf-8').strip())
-            else:
-                words = line.strip().split()
 
-            x = []
-            for w in words:
-                w = [self._word_dicts[i][f] if f in self._word_dicts[i] else 1 for (i,f) in enumerate(w.split('|'))]
-                if len(w) != self._options[0]['factors']:
-                    sys.stderr.write('Error: expected {0} factors, but input word has {1}\n'.format(self._options[0]['factors'], len(w)))
-                    for midx in xrange(self._num_processes):
-                        self._processes[midx].terminate()
-                    sys.exit(1)
-                x.append(w)
-
-            x += [[0]*self._options[0]['factors']]
-
-            queue_item = QueueItem(verbose=self._verbose,
-                                   return_hyp_graph=translation_settings.get_search_graph,
-                                   return_alignment=translation_settings.get_alignment,
-                                   k=translation_settings.beam_width,
-                                   suppress_unk=translation_settings.suppress_unk,
-                                   normalize=translation_settings.normalize,
-                                   nbest=translation_settings.n_best,
-                                   seq=x,
-                                   idx=idx)
-
-            self._input_queue.put(queue_item)
-            source_sentences.append(words)
-        return idx+1, source_sentences
-
-    def _retrieve_jobs(self, num_samples, timeout=5):
-        """
-        """
-        trans = [None] * num_samples
-        out_idx = 0
-        for idx in xrange(num_samples):
-            resp = None
-            while True:
-                try:
-                    resp = self._output_queue.get(True, timeout)
-                # if queue is empty after 5s, check if processes are still alive
-                except Empty:
-                    for midx in xrange(self._num_processes):
-                        if not self._processes[midx].is_alive() and self._processes[midx].exitcode != 0:
-                            # kill all other processes and raise exception if one dies
-                            self._input_queue.cancel_join_thread()
-                            self._output_queue.cancel_join_thread()
-                            for idx in xrange(self._num_processes):
-                                self._processes[idx].terminate()
-                            sys.stderr.write("Error: translate worker process {0} crashed with exitcode {1}".format(self._processes[midx].pid, self._processes[midx].exitcode))
-                            sys.exit(1)
-                    # if processes are okay, break
-                    if resp is not None: break
-            trans[resp[0]] = resp[1]
-            if self._verbose and numpy.mod(idx, 10) == 0:
-                sys.stderr.write('Sample {0} / {1} Done\n'.format((idx+1), num_samples))
-            while out_idx < num_samples and trans[out_idx] != None:
-                yield trans[out_idx]
-                out_idx += 1
+    ### UTILITY FUNCTIONS ###
 
     def _seqs2words(self, cc):
         """
@@ -383,8 +353,75 @@ class Translator(object):
             Translator._print_matrix(hyp, file)
             print >>file, "\n"
 
-    def shutdown(self):
-        self._shutdown.value = True
+    ### WRITING TO AND READING FROM QUEUES ###
+
+    def _send_jobs(self, input_, translation_settings):
+        """
+        """
+        source_sentences = []
+        for idx, line in enumerate(input_):
+            if translation_settings.char_level:
+                words = list(line.decode('utf-8').strip())
+            else:
+                words = line.strip().split()
+
+            x = []
+            for w in words:
+                w = [self._word_dicts[i][f] if f in self._word_dicts[i] else 1 for (i,f) in enumerate(w.split('|'))]
+                if len(w) != self._options[0]['factors']:
+                    sys.stderr.write('Error: expected {0} factors, but input word has {1}\n'.format(self._options[0]['factors'], len(w)))
+                    for midx in xrange(self._num_processes):
+                        self._processes[midx].terminate()
+                    sys.exit(1)
+                x.append(w)
+
+            x += [[0]*self._options[0]['factors']]
+
+            input_item = QueueItem(verbose=self._verbose,
+                                   return_hyp_graph=translation_settings.get_search_graph,
+                                   return_alignment=translation_settings.get_alignment,
+                                   k=translation_settings.beam_width,
+                                   suppress_unk=translation_settings.suppress_unk,
+                                   normalize=translation_settings.normalize,
+                                   nbest=translation_settings.n_best,
+                                   seq=x,
+                                   idx=idx,
+                                   request_id=translation_settings.request_id)
+
+            self._input_queue.put(input_item)
+            source_sentences.append(words)
+        return idx+1, source_sentences
+
+    def _retrieve_jobs(self, num_samples, request_id, timeout=5):
+        """
+        """
+        while len(self._retrieved_translations[request_id]) < num_samples:
+            resp = None
+            while resp is None:
+                try:
+                    resp = self._output_queue.get(True, timeout)
+                # if queue is empty after 5s, check if processes are still alive
+                except Empty:
+                    for midx in xrange(self._num_processes):
+                        if not self._processes[midx].is_alive() and self._processes[midx].exitcode != 0:
+                            # kill all other processes and raise exception if one dies
+                            self._input_queue.cancel_join_thread()
+                            self._output_queue.cancel_join_thread()
+                            for idx in xrange(self._num_processes):
+                                self._processes[idx].terminate()
+                            sys.stderr.write("Error: translate worker process {0} crashed with exitcode {1}".format(self._processes[midx].pid, self._processes[midx].exitcode))
+                            sys.exit(1)
+            request_id, idx, output_item = resp
+            self._retrieved_translations[request_id][idx] = output_item
+            #print self._retrieved_translations
+
+        for idx in xrange(num_samples):
+            yield self._retrieved_translations[request_id][idx]
+
+        # then remove all entries with this request ID from the dictionary, right?
+        del self._retrieved_translations[request_id]
+
+    ### EXPOSED TRANSLATION FUNCTIONS ###
 
     def translate(self, source_segments, translation_settings):
         """
@@ -392,8 +429,10 @@ class Translator(object):
         """
         sys.stderr.write('Translating {0} segments...\n'.format(len(source_segments)))
         n_samples, source_sentences = self._send_jobs(source_segments, translation_settings)
+
         translations = []
-        for i, trans in enumerate([trans for trans in self._retrieve_jobs(n_samples)]):
+        for i, trans in enumerate(self._retrieve_jobs(n_samples, translation_settings.request_id)):
+
             samples, scores, word_probs, alignment, hyp_graph = trans
             # n-best list
             if translation_settings.n_best > 1:
@@ -450,15 +489,18 @@ def main(input_file, output_file, decoder_settings, translation_settings):
     Translates a source language file (or STDIN) into a target language file
     (or STDOUT).
     """
+    translation_settings.request_id = 123
+
     translator = Translator(decoder_settings)
     translations = translator.translate_file(input_file, translation_settings)
-    #TODO: reproduce writing of probs, alignment etc. from original version
-    filecontent = '\n'.join([' '.join(t.target_words) for t in translations])
+    # TODO: reproduce writing of probs, alignment etc. from original version
+    filecontent = '\n'.join([t.target_words for t in translations])
     if output_file is sys.stdout:
-        output_file.write(filecontent)
+        output_file.write(filecontent + '\n')
     else:
         with open(output_file, 'w') as f:
             f.write(filecontent)
+    sys.stderr.write('Done\n')
     translator.shutdown()
 
 
