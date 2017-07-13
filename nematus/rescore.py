54bd7f6283f26198c33216eabc5a4279bb27c2f7
Phil Williams <philip.williams@mac.com>
2017-07-13 10:01:33 +0100

    Fix to 07cebee0... (#49)

--- nematus/rescore.py
+++ nematus/rescore.py
@@ -29,7 +29,7 @@ def rescore_model(source_file, nbest_file, saveto, models, options, b, normaliza
         scores = []
         alignments = []
         for i, model in enumerate(models):
-            f_log_probs = load_scorer.load(model, options[i], alignweights=alignweights)
+            f_log_probs = load_scorer(model, options[i], alignweights=alignweights)
             score, alignment = pred_probs(f_log_probs, prepare_data, options[i], pairs, normalization_alpha=normalization_alpha, alignweights = alignweights)
             scores.append(score)
             alignments.append(alignment)
