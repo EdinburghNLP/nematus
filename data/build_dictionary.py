dd8bc704981272aace589e5608dfa2bee8ac08ef
Bogomil Gospodinov <bgospodinov@outlook.com>
2018-06-07 12:28:06 +0100

    building dictionary updated to py 3

--- data/build_dictionary.py
+++ data/build_dictionary.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python
-
-import numpy
 import json
-
 import sys
-import fileinput
-
 from collections import OrderedDict
 
 def main():
     for filename in sys.argv[1:]:
-        print 'Processing', filename
+        print('Processing', filename)
         word_freqs = OrderedDict()
-        with open(filename, 'r') as f:
+        with open(filename, 'r', encoding='utf-8') as f:
             for line in f:
                 words_in = line.strip().split(' ')
                 for w in words_in:
                     if w not in word_freqs:
                         word_freqs[w] = 0
                     word_freqs[w] += 1
-        words = word_freqs.keys()
-        freqs = word_freqs.values()
-
-        sorted_idx = numpy.argsort(freqs)
-        sorted_words = [words[ii] for ii in sorted_idx[::-1]]
 
         worddict = OrderedDict()
         worddict['eos'] = 0
         worddict['UNK'] = 1
-        for ii, ww in enumerate(sorted_words):
+        for ii, (ww, freq) in enumerate(sorted(word_freqs.items(), key=lambda x: x[1], reverse=True)):
             worddict[ww] = ii+2
 
-        with open('%s.json'%filename, 'wb') as f:
+        with open('%s.json' % filename, 'w', encoding='utf-8') as f:
             json.dump(worddict, f, indent=2, ensure_ascii=False)
 
-        print 'Done'
+        print('Done')
+
 
 if __name__ == '__main__':
     main()
