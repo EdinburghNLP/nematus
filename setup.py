c53d61947e1aeb1686beee1a9881ba58928ee01e
Rico Sennrich <rico.sennrich@gmx.ch>
2019-05-23 16:45:57 +0100

    fix install on systems with non-utf8 env

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 import os
+import io
 import setuptools
 
 setuptools.setup(
     name='nematus',
     version='0.4',
     description='Neural machine translation tools on top of Tensorflow',
-    long_description=open(os.path.join(os.path.dirname(
-        os.path.abspath(__file__)), 'README.md')).read(),
+    long_description=io.open(os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), 'README.md'),encoding='UTF-8').read(),
     license='BSD 3-clause',
     url='http://github.com/EdinburghNLP/nematus',
     install_requires=['numpy',
