4b8aecf3710495c3fbc17db7fd024219915af54e
zippotju <s1684872@sms.ed.ac.uk>
2019-10-03 21:41:26 +0100

    enable to set maximum vocabulary size
    Enable to set the maximum vocabulary size when building the vocabulary

--- data/build_dictionary.py
+++ data/build_dictionary.py
@@ -9,7 +9,14 @@ import json
 
 
 def main():
-    for filename in sys.argv[1:]:
+    if sys.argv[-1].isdigit():
+        vsize = int(sys.argv[-1])
+    elif sys.argv[-1] == 'False':
+        vsize = False
+    else:
+        raise ValueError("need to specify maximum vocabulary size. Set to 'False' if no limitation")
+
+    for filename in sys.argv[1:-1]:
         print('Processing', filename)
         word_freqs = OrderedDict()
         with open(filename, 'r', encoding='utf-8') as f:
@@ -30,8 +37,14 @@ def main():
         worddict['<GO>'] = 1
         worddict['<UNK>'] = 2
         # FIXME We shouldn't assume <EOS>, <GO>, and <UNK> aren't BPE subwords.
-        for ii, ww in enumerate(sorted_words):
-            worddict[ww] = ii+3
+        if vsize:
+            for ii, ww in enumerate(sorted_words):
+                if len(worddict) == vsize:
+                    break
+                worddict[ww] = ii + 3
+        else:
+            for ii, ww in enumerate(sorted_words):
+                worddict[ww] = ii + 3
 
         # The JSON RFC requires that JSON text be represented using either
         # UTF-8, UTF-16, or UTF-32, with UTF-8 being recommended.
