f7947818c1e9a4bd60c8356852f583b33c924722
zippotju <s1684872@sms.ed.ac.uk>
2019-10-15 21:53:41 +0100

    Update transformer.py

--- nematus/transformer.py
+++ nematus/transformer.py
@@ -346,7 +346,7 @@ class TransformerDecoder(object):
         def _decode_all(target_embeddings):
             """ Decodes the encoder-generated representations into target-side logits in parallel. """
             # Propagate inputs through the encoder stack
-            dec_output = dec_input
+            dec_output = target_embeddings
             for layer_id in range(1, self.config.transformer_dec_depth + 1):
                 dec_output, _ = self.decoder_stack[layer_id]['self_attn'].forward(dec_output, None, self_attn_mask)
                 dec_output, _ = \
