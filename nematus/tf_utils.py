b1cbc9a19e094cb4df5b155d44a3cde9e71dd7d4
Phil Williams <philip.williams@mac.com>
2019-11-01 09:03:08 +0000

    Skip assert_shapes check if TensorFlow is too old

--- nematus/tf_utils.py
+++ nematus/tf_utils.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
 
 def assert_shapes(shapes):
-    assertion_op = tf.debugging.assert_shapes(shapes)
-    with tf.control_dependencies([assertion_op]):
+    # tf.debugging.assert_shapes is only supported in 1.14 and later, so
+    # the call is wrapped in a try-except to allow Nematus to run on earlier
+    # versions.
+    try:
+        assertion_op = tf.debugging.assert_shapes(shapes)
+        with tf.control_dependencies([assertion_op]):
+            pass
+    except AttributeError:
         pass
